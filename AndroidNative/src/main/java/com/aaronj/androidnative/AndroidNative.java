171961b31a669586cd1e4faddf2f9ab1df0456c4
Aaron8052 <jiangyu8052@gmail.com>
2022-11-07 12:34:16 -0500

    完善插件功能

--- AndroidNative/src/main/java/com/aaronj/androidnative/AndroidNative.java
+++ AndroidNative/src/main/java/com/aaronj/androidnative/AndroidNative.java
@@ -1,13 +1,25 @@
 package com.AaronJ.androidnative;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Locale;
 
 
-public class AndroidNative extends AppCompatActivity {
-
+public class AndroidNative extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
     static AndroidNative instance;
 
     public static AndroidNative getInstance(){
@@ -16,32 +28,80 @@ public class AndroidNative extends AppCompatActivity {
         return instance;
     }
 
-    public void Share(String title, String message, String url, String imagePath){
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        if(imagePath.isEmpty()){
-            intent.setType("text/plain");
+
+    //region Share
+    public void shareTextAndImage(String title, String message, String url, String imagePath){
+        Intent intent = new Intent(Intent.ACTION_SEND);//Create Send Action
+        intent.putExtra(Intent.EXTRA_TITLE, message); //Add message as title
+        intent.putExtra(Intent.EXTRA_TEXT, url);//Add url as text for user to copy
+
+        File imageFile = new File(imagePath);//Get image file from path
+        if(imageFile != null){
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(imageFile));//Add image file by path
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//Grant read file permission
+
+            //Get and set file extension (Type)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {  //For sdk version higher than 26
+                try {
+                    intent.setType("image/"+ Files.probeContentType(imageFile.toPath()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{   //Fallback
+                String fileName = imageFile.getName();
+                intent.setType("image/"+ fileName.substring(fileName.lastIndexOf('.')+1));
+            }
         }
-        else{
-            intent.setType("image/png");
-            intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(imagePath));
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        }
 
-        intent.putExtra(Intent.EXTRA_TITLE, message);
-        intent.putExtra(Intent.EXTRA_TEXT, url);
-
-
-        Intent.createChooser(intent,"Share Score");
-        startActivity(Intent.createChooser(intent,"Share Score"));
+        startActivity(Intent.createChooser(intent, title));//Show Sharing Panel
     }
 
-    public void Share(String message){
-        Share("Share", message, null,null);
+    public void shareText(String message){
+        Intent intent = new Intent(Intent.ACTION_SEND);//Create Send Action
+        intent.putExtra(Intent.EXTRA_TEXT, message);//Add message as text for user to copy
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent,null));//Show Sharing Panel
     }
-    public void Share(String title, String message){
-        Share(title, message, null,null);
+    public void shareTextWithTitle(String title, String message){
+        Intent intent = new Intent(Intent.ACTION_SEND);//Create Send Action
+        intent.putExtra(Intent.EXTRA_TEXT, message);//Add message as text for user to copy
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, title));//Show Sharing Panel
     }
-    public void Share(String title, String message, String url){
-        Share(title, message, url,null);
+    public void shareTextWithURL(String title, String message, String url){
+        Intent intent = new Intent(Intent.ACTION_SEND);//Create Send Action
+        intent.putExtra(Intent.EXTRA_TITLE, message); //Add message as title
+        intent.putExtra(Intent.EXTRA_TEXT, url);//Add url as text for user to copy
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, title));//Show Sharing Panel
     }
+    //endregion
+
+    //region Utilities
+    Context context;
+    Context getContext(){
+        if(context == null)
+            context = this.getContext();
+        return context;
+    }
+
+
+    public String getISOCountry(){
+        return Locale.getDefault().getCountry();
+    }
+    public String getISOLanguage(){
+        return Locale.getDefault().getLanguage();
+    }
+
+    public float getBatteryPercentage(){
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getContext().registerReceiver(null, ifilter);
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level * 100 / (float)scale;
+        return batteryPct;
+    }
+    //endregion
 }
